a89a961a9043edaf43461c3b2bb1f2d63e360f34
Pavel Klimov <pavelklimovmail@yandex.ru>
2019-05-10 21:11:58 +0300

    Справил проблемы со stile и viewBox

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -4,13 +4,13 @@ const MainPage = () => {
   return <>
 <div style={{display: `none`}}>
   <svg xmlns="http://www.w3.org/2000/svg">
-    <symbol id="icon-arrow-select" viewbox="0 0 7 4">
+    <symbol id="icon-arrow-select" viewBox="0 0 7 4">
       <path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path>
     </symbol>
-    <symbol id="icon-bookmark" viewbox="0 0 17 18">
+    <symbol id="icon-bookmark" viewBox="0 0 17 18">
       <path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path>
     </symbol>
-    <symbol id="icon-star" viewbox="0 0 13 12">
+    <symbol id="icon-star" viewBox="0 0 13 12">
       <path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path>
     </symbol>
   </svg>
@@ -128,7 +128,7 @@ const MainPage = () => {
               </div>
               <div className="place-card__rating rating">
                 <div className="place-card__stars rating__stars">
-                  <span style="width: 93%"></span>
+                  <span style={{width: `93%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
               </div>
@@ -159,7 +159,7 @@ const MainPage = () => {
               </div>
               <div className="place-card__rating rating">
                 <div className="place-card__stars rating__stars">
-                  <span style="width: 80%"></span>
+                  <span style={{width: `80%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
               </div>
@@ -191,7 +191,7 @@ const MainPage = () => {
               </div>
               <div className="place-card__rating rating">
                 <div className="place-card__stars rating__stars">
-                  <span style="width: 80%"></span>
+                  <span style={{width: `80%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
               </div>
@@ -226,7 +226,7 @@ const MainPage = () => {
               </div>
               <div className="place-card__rating rating">
                 <div className="place-card__stars rating__stars">
-                  <span style="width: 100%"></span>
+                  <span style={{width: `100%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
               </div>
@@ -257,7 +257,7 @@ const MainPage = () => {
               </div>
               <div className="place-card__rating rating">
                 <div className="place-card__stars rating__stars">
-                  <span style="width: 80%"></span>
+                  <span style={{width: `80%`}}></span>
                   <span className="visually-hidden">Rating</span>
                 </div>
               </div>
